9fc24366c456e40c567685e472c05f8ee3a6890a
devkeita <keita.mywork@gmail.com>
2020-11-28 11:09:39 +0900

    add google api library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { AuthProvider } from "../contexts/Auth";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
     return (
-        <AuthProvider>
-            <Component {...pageProps} />
-        </AuthProvider>
+        <React.Fragment>
+            <Head>
+                <script src="https://apis.google.com/js/api.js"></script>
+            </Head>
+            <AuthProvider>
+                <Component {...pageProps} />
+            </AuthProvider>
+        </React.Fragment>
     );
 };
 
