ccc368d82a332a682382de87caf895149b733379
devkeita <keita.mywork@gmail.com>
2020-11-28 11:58:38 +0900

    remove google api library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { AuthProvider } from "../contexts/Auth";
-import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
     return (
         <React.Fragment>
-            <Head>
-                <script src="https://apis.google.com/js/api.js"></script>
-            </Head>
             <AuthProvider>
                 <Component {...pageProps} />
             </AuthProvider>
